a0fb67305fb12dbee58d5bceb88837484c20744e
Chadwick Flores <chadwickflores@gmail.com>
2017-03-12 15:11:58 -0500

    started mailer setup for password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" }
+
+  # Users routes
   get  "/my_current_user" => "users#my_current_user"
   match 'users/:id' => 'users#update_user', via: [:patch]
+  get '/send_password' => "users#reset_password"
 end
