8fa801f02a91e9ce9ebdd424d1921b67e61bb3da
Chadwick Flores <chadwickflores@gmail.com>
2017-03-12 14:55:03 -0500

    created users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations" }
+  get  "/my_current_user" => "users#my_current_user"
+  match 'users/:id' => 'users#update_user', via: [:patch]
 end
