d8a56a2d0851f82d608945b681589d42f7448e85
Chadwick Flores <chadwickflores@gmail.com>
2017-03-15 14:51:43 -0500

    created progress model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314142703) do
+ActiveRecord::Schema.define(version: 20170315175008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 20170314142703) do
     t.string   "thumbnail"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "progresses", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "chapter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chapter_id"], name: "index_progresses_on_chapter_id", using: :btree
+    t.index ["user_id"], name: "index_progresses_on_user_id", using: :btree
   end
 
   create_table "schools", force: :cascade do |t|
